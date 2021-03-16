339b199207d6e142cd233ba1d335ea0e0b34b005
Maicon Siqueira <maicon.siqueira@semantix.com.br>
2021-03-15 23:23:58 -0300

    chore: initial chalange

--- reports_gen/lib/parser.ex
+++ reports_gen/lib/parser.ex
@@ -3,16 +3,15 @@ defmodule ReportsGen.Parser do
     line
     |> String.trim()
     |> String.split(",")
-    |> line_update(4) 
+    |> line_update(4)
   end
 
-    def line_update(line, index) when index <= 1 do 
-      List.update_at(line, index, fn elem -> String.to_integer(elem) end)
-    end
+  def line_update(line, index) when index <= 1 do
+    List.update_at(line, index, fn elem -> String.to_integer(elem) end)
+  end
 
-    def line_update(line, index) do 
-       List.update_at(line, index, fn elem -> String.to_integer(elem) end)
-       |> line_update(index - 1)
-    end
-
+  def line_update(line, index) do
+    List.update_at(line, index, fn elem -> String.to_integer(elem) end)
+    |> line_update(index - 1)
+  end
 end
--- reports_gen/lib/reports_gen.ex
+++ reports_gen/lib/reports_gen.ex
@@ -14,17 +14,37 @@ defmodule ReportsGen do
     "Vinicius"
   ]
 
+  @available_month [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+  ]
+
+  @available_years [
+    2016, 
+    2017, 
+    2018, 
+    2019, 
+    2020
+  ]
+
   def readfile(file_name) do
     File.stream!("reports/#{file_name}")
-    #|> Enum.map(fn line -> Parser.parse_line(line) end)
-    |> Enum.reduce(map_users(), fn line, acc -> 
+    # |> Enum.map(fn line -> Parser.parse_line(line) end)
+    |> Enum.reduce(map_users(), fn line, acc ->
       [user, hr, _dia, _mes, _ano] = Parser.parse_line(line)
-      Map.put(acc, user, hr + acc[user]) 
-      end)
+      Map.put(acc, user, hr + acc[user])
+    end)
   end
 
-  def map_users() do
-    Enum.into(@available_users, %{}, fn x -> {x, 0} end)
+  def map_users do
+    users = Enum.into(@available_users, %{}, fn x -> {x, 0} end)
+    month = Enum.into(@available_month, %{}, fn x -> {x, 0} end)
+    years = Enum.into(@available_years, %{}, fn x -> {x, 0} end)
+
+    %{
+      "users" => users,
+      "month" => month,
+      "years" => years
+    }
   end
 
 end
